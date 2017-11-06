0f9b1a6607479833e2542346df61915f69b0099a
b21627672 <32526590+b21627672@users.noreply.github.com>
2017-11-06 18:11:58 +0300

    UPDATED

--- 1.py
+++ 1.py
@@ -6,37 +6,100 @@ a) performs data cleaning process to remove missing attribute values present in 
 b) calculates probability of being breast cancer of an imaginary patient
    by evaluationg his/her sample results provided as command-line argument.
 '''
+#START OF 1.PY
 
-# Starter code that reads database named 'WBC.data' and loads it into a dictionary 'dataDic'
+import sys
+import copy
 
-# Reads the datafile. Note: WBC.data should be located where this file belongs.
-dataFile = open('WBC.data','r').read()
+dataFile = open('WBC.data', 'r').read()
+dataDic = {i.split(',')[0]: i.split(',')[1:] for i in dataFile.split('\n')}
+for ids in dataDic:
+    for indx, value in enumerate(dataDic[ids]):
+       if value.isdigit():
+           dataDic[ids][indx] = int(value)
 
-# Makes data file ready to use by assigning every record to a dictionary class name dataDic.
-dataDic = {i.split(',')[0]: i.split(',')[1:]  for i in dataFile.split('\n')} 
-
-# Do not alter any upper lines so that you do not get trouble in loading data file
-
-# Performs data cleaning process, design the content and arguments depending on your design
 def funDataClean():
+    total = 0
+    dataDic_copy = copy.deepcopy(dataDic)
+    for j in sorted(dataDic):
+        attribute_values = []
+        if "?" in dataDic[j]:
+            class_canser = dataDic[j][9]
+            question_index = dataDic[j].index("?")
+            for i in sorted(dataDic):
+                if dataDic[i][9] != class_canser:
+                    dataDic_copy.pop(i)
+            for k in sorted(dataDic_copy):
+                if type(dataDic_copy[k][question_index]) is int:
+                    attribute_values.append(dataDic_copy[k][question_index])
+            AV = round(sum(attribute_values) / len(attribute_values))
+            dataDic[j][question_index] = AV
+            total += AV
+            dataDic_copy = copy.deepcopy(dataDic)
+    average = total / 21
+    print('The average of all missing values is  : ' + '{0:.4f}'.format(average))
 
+y = sys.argv[1].split(",")
+patient_data = []
+for i in y:
+    if "?" is i:
+        patient_data.append(list(i))
+    else:
+        patient_data.append(i.split(":"))
+for i in patient_data:
+    if "?" not in i:
+        i[1] = int(i[1])
 
-# Performas step-wise search in WBC database, design the content and arguments depending on your design
 def performStepWiseSearch():
+    prob = []
+    m = 0
+    b = 0
+    index = 0
+    for i in patient_data:
+        if "?" in i:
+            for j in sorted(dataDic):
+                prob.append(j)
+        elif "<" in i and "<=" not in i:
+            for j in dataDic:
+                if dataDic[j][index] < i[1]:
+                    prob.append(j)
+        elif "<=" in i:
+            for j in dataDic:
+                if dataDic[j][index] <= i[1]:
+                    prob.append(j)
+        elif ">" in i and ">=" not in i:
+            for j in dataDic:
+                if dataDic[j][index] > i[1]:
+                    prob.append(j)
+        elif ">=" in i:
+            for j in dataDic:
+                if dataDic[j][index] >= i[1]:
+                    prob.append(j)
+        elif "!=" in i:
+            for j in dataDic:
+                if dataDic[j][index] != i[1]:
+                    prob.append(j)
+        elif "=" in i and "!=" not in i:
+            for j in dataDic:
+                if dataDic[j][index] == i[1]:
+                    prob.append(j)
+        index +=1
 
+    for k in sorted(dataDic):
+        if prob.count(k) == 9:
+            if dataDic[k][9] == "malignant":
+                m += 1
+            elif dataDic[k][9] == "benign":
+                b += 1
+    probability = m / (m + b)
+    print('\nTest Results:\n'
+          '----------------------------------------------'
+          '\nPositive (malignant) cases            : ' + str(m) +
+          '\nNegative (benign) cases               : ' + str(b) +
+          '\nThe probability of being positive     : ' + '{0:.4f}'.format(probability) +
+          '\n----------------------------------------------')
 
-# 1st phase: Cleaning WBC Database
+funDataClean()
+performStepWiseSearch()
 
-print('The average of all missing values is  : ' + '{0:.4f}'.format())
-
-# 2nd phase: Retrieving knowledge from WBC dataset
-
-print('\nTest Results:\n'
-      '----------------------------------------------'
-      '\nPositive (malignant) cases            : ' + str() +
-      '\nNegative (benign) cases               : ' + str() +
-      '\nThe probability of being positive     : ' + '{0:.4f}'.format() +
-      '\n----------------------------------------------')
-	  
-	  
-# end of the 1.py file
+#end of 1.py file
